662488a944f3af7ae720c143b68c2ac68e503bf0
Gildas <gildas.lormeau@gmail.com>
2021-12-03 01:12:50 +0100

    remove init() function

--- src/background.js
+++ src/background.js
@@ -43,62 +43,6 @@ function refreshMenuEntry() {
 	}
 }
 
-function init() {
-	chrome.runtime.onConnect.addListener(function (port) {
-		port.onMessage.addListener(function (msg) {
-			var workerFormatter, workerJSONLint, json = msg.json;
-
-			function onWorkerJSONLintMessage() {
-				var message = JSON.parse(event.data);
-				workerJSONLint.removeEventListener("message", onWorkerJSONLintMessage, false);
-				workerJSONLint.terminate();
-				port.postMessage({
-					ongetError: true,
-					error: message.error,
-					loc: message.loc,
-					offset: msg.offset
-				});
-			}
-
-			function onWorkerFormatterMessage(event) {
-				var message = event.data;
-				workerFormatter.removeEventListener("message", onWorkerFormatterMessage, false);
-				workerFormatter.terminate();
-				if (message.html)
-					port.postMessage({
-						onjsonToHTML: true,
-						html: message.html,
-						theme: localStorage.theme
-					});
-				if (message.error) {
-					workerJSONLint = new Worker("workerJSONLint.js");
-					workerJSONLint.addEventListener("message", onWorkerJSONLintMessage, false);
-					workerJSONLint.postMessage(json);
-				}
-			}
-
-			if (msg.init)
-				port.postMessage({
-					oninit: true,
-					options: localStorage.options ? JSON.parse(localStorage.options) : {}
-				});
-			if (msg.copyPropertyPath) {
-				path = msg.path;
-				value = msg.value;
-			}
-			if (msg.jsonToHTML) {
-				workerFormatter = new Worker("workerFormatter.js");
-				workerFormatter.addEventListener("message", onWorkerFormatterMessage, false);
-				workerFormatter.postMessage({
-					json: json,
-					fnName: msg.fnName
-				});
-			}
-		});
-	});
-	refreshMenuEntry();
-}
-
 var options = {};
 if (localStorage.options)
 	options = JSON.parse(localStorage.options);
@@ -110,11 +54,63 @@ if (typeof options.addContextMenu == "undefined") {
 if (!localStorage.theme)
 	getDefaultTheme().then(theme => {
 		localStorage.theme = theme;
-		init();
+		refreshMenuEntry();
 	});
 else
-	init();
+	refreshMenuEntry();
 
+chrome.runtime.onConnect.addListener(function (port) {
+	port.onMessage.addListener(function (msg) {
+		var workerFormatter, workerJSONLint, json = msg.json;
+
+		function onWorkerJSONLintMessage(event) {
+			var message = JSON.parse(event.data);
+			workerJSONLint.removeEventListener("message", onWorkerJSONLintMessage, false);
+			workerJSONLint.terminate();
+			port.postMessage({
+				ongetError: true,
+				error: message.error,
+				loc: message.loc,
+				offset: msg.offset
+			});
+		}
+
+		function onWorkerFormatterMessage(event) {
+			var message = event.data;
+			workerFormatter.removeEventListener("message", onWorkerFormatterMessage, false);
+			workerFormatter.terminate();
+			if (message.html)
+				port.postMessage({
+					onjsonToHTML: true,
+					html: message.html,
+					theme: localStorage.theme
+				});
+			if (message.error) {
+				workerJSONLint = new Worker("workerJSONLint.js");
+				workerJSONLint.addEventListener("message", onWorkerJSONLintMessage, false);
+				workerJSONLint.postMessage(json);
+			}
+		}
+
+		if (msg.init)
+			port.postMessage({
+				oninit: true,
+				options: localStorage.options ? JSON.parse(localStorage.options) : {}
+			});
+		if (msg.copyPropertyPath) {
+			path = msg.path;
+			value = msg.value;
+		}
+		if (msg.jsonToHTML) {
+			workerFormatter = new Worker("workerFormatter.js");
+			workerFormatter.addEventListener("message", onWorkerFormatterMessage, false);
+			workerFormatter.postMessage({
+				json: json,
+				fnName: msg.fnName
+			});
+		}
+	});
+});
 chrome.runtime.onMessage.addListener(message => {
 	if (message == "refreshMenuEntry")
 		refreshMenuEntry();
