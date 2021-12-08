d595d3c3beeae33df318d94bfa08bd44fe50c85e
Gildas <gildas.lormeau@gmail.com>
2021-12-09 00:33:14 +0100

    ignore Haskell files (fix #79)

--- src/js/content.js
+++ src/js/content.js
@@ -48,7 +48,10 @@ function extractJsonInfo(rawText) {
 	}
 
 	function detectJson(text) {
-		return ((text.charAt(0) == "[" && text.charAt(text.length - 1) == "]") || (text.charAt(0) == "{" && text.charAt(text.length - 1) == "}"));
+		return (
+			(text.charAt(0) == "[" && text.charAt(text.length - 1) == "]") ||
+			(text.charAt(0) == "{" && text.charAt(1) != "-" && text.charAt(text.length - 1) == "}")
+		);
 	}
 }
 
